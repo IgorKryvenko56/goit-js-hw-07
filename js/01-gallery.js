0b23141084b5d4e050975336b64b11585ee85bd6
IgorKryvenko56 <ikryvenko@yahoo.co.uk>
2023-03-28 22:58:35 +0300

    HW07 task01 First attempt

---
+++ js/01-gallery.js
@@ -0,0 +1,65 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+// 
+
+ const gallery = document.querySelector('.gallery');
+ const imgs = galleryItems.map(item => `
+ <li class='gallery__item'>
+ <a class='gallery__link' href='${item.original}'>
+ <img class='gallery__image' src='${item.preview}'
+ data-source='${item.original}' alt='${item.description}' /></a>
+ </li>`).join('');
+
+ gallery.insertAdjacentHTML('beforeend', imgs);
+ gallery.addEventListener('click', (evt) => {
+   evt.preventDefault();
+   if (evt.target.classList.contains('gallery_image')) {
+      const imgSrc = evt.target.dataset.source;
+      console.log(imgSrc);
+   }
+ });
+
+
+
+//  import * as basicLightbox from 'basiclightbox';
+ const instance = basicLightbox.create(
+   `<img src="" width="800" height="600">`, {
+      onShow: (instance) => {
+          gallery.addEventListener("keydown", onEscKeyPress);
+          console.log("open");
+       },
+     onClose: (instance) => {
+          gallery.removeEventListener("keydown", onEscKeyPress);
+          console.log("close");
+       },
+   });
+
+  const clickOnImage = (evt) => {
+       evt.preventDefault();
+  const imgSrc = evt.target.dataset.source;
+      if (imgSrc) {
+        instance.element().querySelector("img").src = imgSrc;
+           instance.show();
+       }
+       return;
+   };
+
+   const onEscKeyPress = (evt) => {
+       console.log(evt);
+      const ESC_KEY_CODE = "Escape";
+       const IsEscKeY = evt.code === ESC_KEY_CODE;
+       if (IsEscKeY) {
+          instance.onClose();
+       }
+   };
+
+gallery.addEventListener("click", clickOnImage);
+
+
+
+
+
+
+
+
+
